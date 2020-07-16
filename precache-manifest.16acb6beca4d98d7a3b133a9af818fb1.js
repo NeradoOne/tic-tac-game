15b7c4191b36b937208dd313e215fb0ba11cf5eb
NeradoOne <nerado.one@gmail.com>
2020-07-16 17:17:53 -0300

    Updates

---
+++ precache-manifest.16acb6beca4d98d7a3b133a9af818fb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f45c840841056706b79e0708169470a",
+    "url": "/tic-tac-game/index.html"
+  },
+  {
+    "revision": "d08d8afb34ee26ef78cf",
+    "url": "/tic-tac-game/static/css/main.bc276482.chunk.css"
+  },
+  {
+    "revision": "7de1f036a2157dac2058",
+    "url": "/tic-tac-game/static/js/2.57343eaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-game/static/js/2.57343eaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d08d8afb34ee26ef78cf",
+    "url": "/tic-tac-game/static/js/main.628fa5d0.chunk.js"
+  },
+  {
+    "revision": "646d4d3fa23c5edaa5c5",
+    "url": "/tic-tac-game/static/js/runtime-main.d7aba191.js"
+  }
+]);
